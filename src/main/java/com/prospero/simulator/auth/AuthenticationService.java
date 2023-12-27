e20033ab81063201aee0673be63f13b82a1e18b0
Ratmir Mukazhanov <ratmir.m2004@gmail.com>
2023-12-27 22:38:31 +0000

    Spring Security authentication set up

---
+++ src/main/java/com/prospero/simulator/auth/AuthenticationService.java
@@ -0,0 +1,48 @@
+package com.prospero.simulator.auth;
+
+import com.prospero.simulator.config.JwtService;
+import com.prospero.simulator.user.User;
+import com.prospero.simulator.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(request.getRole())
+                .build();
+        repository.save(user);
+        var token = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(token)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = repository.findByEmail(request.getEmail()).orElseThrow();
+        var token = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(token)
+                .build();
+    }
+
+}
---
+++ src/main/java/com/prospero/simulator/user/Role.java
@@ -0,0 +1,7 @@
+package com.prospero.simulator.user;
+
+public enum Role {
+    USER,
+    MODERATOR,
+    ADMIN
+}
